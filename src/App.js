bc37fdb7473f21f11584cde619a1856a0d617b30
KartikKonje30 <kartikkonje.01@gmail.com>
2024-04-04 20:23:56 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./styles.css";
+
+import React, { useState } from 'react';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <div>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+      </div>
+    </div>
+  );
+}
+
+
